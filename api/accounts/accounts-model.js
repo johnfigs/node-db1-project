c132b3fc1dbea36a269e593db3290e44b6af78d9
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-10-04 19:57:18 -0700

    Completed model functions

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,44 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
   // DO YOUR MAGIC
+  //SELECT * FROM Accounts
+  return db('accounts')
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  //SELECT * FROM Accounts WHERE id = X
+  return db('accounts')
+    .where('id', id)
+    .first()
 }
 
-const create = account => {
+const create = async account => {
   // DO YOUR MAGIC
+  // INSERT INTO Accounts ('name', 'budget') VALUES ('newname', 'newbudget')
+  const [id] = await db('accounts')
+    .insert(account)
+  return getById(id)
 }
 
-const updateById = (id, account) => {
+const updateById = async (id, account) => {
   // DO YOUR MAGIC
+  // UPDATE Accounts SET name = 'foo', account = 'bar' WHERE id = X
+  await db('accounts')
+    .update(account)
+    .where('id', id)
+  return getById(id)
 }
 
-const deleteById = id => {
+const deleteById = async id => {
   // DO YOUR MAGIC
+  // DELETE FROM Accounts WHERE id = X
+  const removed = await getById(id)
+  await db('accounts')
+    .where('id', id)
+    .delete()
+  return removed
 }
 
 module.exports = {
